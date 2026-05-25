6664c251a7c5112b4ed22436f95d82268561c8b4
bsorrentino <bartolomeo.sorrentino@gmail.com>
2026-05-25 21:53:53 +0200

    test(StateSubGraphTest): add test for simple state subgraph execution

--- langgraph4j-core/src/test/java/org/bsc/langgraph4j/StateSubGraphTest.java
+++ langgraph4j-core/src/test/java/org/bsc/langgraph4j/StateSubGraphTest.java
@@ -88,6 +88,64 @@ public class StateSubGraphTest {
                 .map(NodeOutput::node)
                 .toList();
     }
+    @Test
+    public void testStateSubgraphSimple() throws Exception {
+
+        AsyncNodeActionWithConfig<GraphTest.State> childStep1 =
+                AsyncNodeActionWithConfig.node_async((state, config) ->
+                        Map.of("messages", "child:step1"));
+
+        AsyncNodeActionWithConfig<GraphTest.State> childStep2 =
+                AsyncNodeActionWithConfig.node_async((state, config) ->
+                        Map.of("messages", "child:step2"));
+
+        AsyncNodeActionWithConfig<GraphTest.State> childStep3 =
+                AsyncNodeActionWithConfig.node_async((state, config) ->
+                        Map.of("messages", "child:step3"));
+
+        var workflowChild = new StateGraph<>(GraphTest.State.SCHEMA, GraphTest.State::new)
+                .addNode("child:step_1", childStep1)
+                .addNode("child:step_2", childStep2)
+                .addNode("child:step_3", childStep3)
+                .addEdge(START, "child:step_1")
+                .addEdge("child:step_1", "child:step_2")
+                .addEdge("child:step_2", "child:step_3")
+                .addEdge("child:step_3", END)
+                ;
+        AsyncNodeActionWithConfig<GraphTest.State> step1 =
+                AsyncNodeActionWithConfig.node_async((state, config) ->
+                        Map.of("messages", "step1"));
+
+        AsyncNodeActionWithConfig<GraphTest.State> step2 =
+                AsyncNodeActionWithConfig.node_async((state, config) ->
+                        Map.of("messages", "step2"));
+
+        AsyncNodeActionWithConfig<GraphTest.State> step3 =
+                AsyncNodeActionWithConfig.node_async((state, config) ->
+                        Map.of("messages", "step3"));
+
+        var workflowParent = new StateGraph<>(GraphTest.State.SCHEMA, GraphTest.State::new)
+                .addNode("step_1", step1)
+                .addNode("step_2", step2)
+                .addNode("step_3", step3)
+                .addNode("subgraph", workflowChild)
+                .addEdge(START, "step_1")
+                .addEdge("step_1", "step_2")
+                .addEdge("step_2", "subgraph")
+                .addEdge("subgraph", "step_3")
+                .addEdge("step_3", END)
+                .compile();
+
+        var result = workflowParent.stream(GraphInput.noArgs(), RunnableConfig.empty())
+                .stream()
+                .peek(System.out::println)
+                .reduce((a, b) -> b)
+                .map(NodeOutput::state);
+
+        assertTrue(result.isPresent());
+        assertIterableEquals(List.of("step1", "step2", "child:step1", "child:step2", "child:step3", "step3"), result.get().messages());
+
+    }
 
 
     @Test
