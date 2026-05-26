f6988893eb9b6bb19d4ce65d775481751c8344a3
bsorrentino <bartolomeo.sorrentino@gmail.com>
2026-05-26 10:23:10 +0200

    test(core): update expected result

--- langgraph4j-core/src/test/java/org/bsc/langgraph4j/InterruptionTest.java
+++ langgraph4j-core/src/test/java/org/bsc/langgraph4j/InterruptionTest.java
@@ -344,7 +344,7 @@ public class InterruptionTest {
                 .toList();
         assertIterableEquals(List.of(
                 "A",
-                //"A",
+                "A",
                 "A"
         ), results );
 
