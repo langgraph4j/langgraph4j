aff46a110bc3fc4311983448a4484110d8cfbd65
bsorrentino <bartolomeo.sorrentino@gmail.com>
2026-05-27 15:27:14 +0200

    feat(MemorySaver): implement tagging functionality for checkpoints with versioning support
    work on #392

--- langgraph4j-core/src/main/java/org/bsc/langgraph4j/checkpoint/MemorySaver.java
+++ langgraph4j-core/src/main/java/org/bsc/langgraph4j/checkpoint/MemorySaver.java
@@ -4,8 +4,13 @@ import org.bsc.langgraph4j.RunnableConfig;
 
 import java.util.*;
 
+import static java.util.Objects.requireNonNull;
+import static java.util.Optional.ofNullable;
+
 public class MemorySaver extends AbstractCheckpointSaver {
+
     private final Map<String, LinkedList<Checkpoint>> _checkpointsByThread = new HashMap<>();
+    private final Map<String, TreeMap<Integer,Tag>> _tagsByThread = new HashMap<>();
 
     protected final Map<String, LinkedList<Checkpoint>> cache() {
         return Map.copyOf(_checkpointsByThread);
@@ -19,10 +24,27 @@ public class MemorySaver extends AbstractCheckpointSaver {
     protected final void updatedCheckpoint( RunnableConfig config, LinkedList<Checkpoint> checkpoints, Checkpoint checkpoint) throws Exception {
     }
 
+    private Optional<Integer> lastVersion( TreeMap<Integer,Tag> prevTagsByThread) {
+        return ofNullable(prevTagsByThread.lastEntry())
+                .map(Map.Entry::getKey);
+    }
+
     @Override
     protected final Tag releaseCheckpoints(RunnableConfig config, LinkedList<Checkpoint> checkpoints) throws Exception {
         final var threadId = threadId(config);
-        return new Tag( threadId(config), _checkpointsByThread.remove( threadId ) );
+
+        final var prevTagsByThread = _tagsByThread
+                .computeIfAbsent( threadId, k -> new TreeMap<>() );
+
+        var lastThreadVersion = lastVersion(prevTagsByThread).orElse(0);
+
+        final var tag =  new Tag( threadId(config),
+                                    ++lastThreadVersion,
+                                    _checkpointsByThread.remove( threadId ) );
+
+        prevTagsByThread.put( lastThreadVersion, tag );
+
+        return tag;
     }
 
     @Override
@@ -33,4 +55,14 @@ public class MemorySaver extends AbstractCheckpointSaver {
 
     }
 
+    @Override
+    public Optional<Tag> tag(RunnableConfig config, Integer version) throws Exception {
+        requireNonNull(config, "config cannot be null");
+        return ofNullable(_tagsByThread.get(threadId(config)))
+                .map( tagsByVersion ->
+                        ofNullable(version).map(tagsByVersion::get)
+                                .orElseGet( () -> tagsByVersion.lastEntry().getValue() ));
+
+
+    }
 }
