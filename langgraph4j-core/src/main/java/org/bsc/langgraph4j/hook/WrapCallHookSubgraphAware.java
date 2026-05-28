a6f394961586caec91fc62eb853060a9dc210b22
bsorrentino <bartolomeo.sorrentino@gmail.com>
2026-05-28 20:37:15 +0200

    refactor(WrapCallHookSubgraphAware): simplify subgraph stack management by using node IDs

--- langgraph4j-core/src/main/java/org/bsc/langgraph4j/hook/WrapCallHookSubgraphAware.java
+++ langgraph4j-core/src/main/java/org/bsc/langgraph4j/hook/WrapCallHookSubgraphAware.java
@@ -1,7 +1,6 @@
 package org.bsc.langgraph4j.hook;
 
 import org.bsc.async.AsyncGenerator;
-import org.bsc.langgraph4j.GraphPath;
 import org.bsc.langgraph4j.RunnableConfig;
 import org.bsc.langgraph4j.state.AgentState;
 
@@ -10,28 +9,22 @@ import java.util.*;
 public abstract class WrapCallHookSubgraphAware<S extends AgentState> implements NodeHook.WrapCall<S> {
         protected record Step(String nodeId, String lastPathElement ) {}
 
-        private final Deque<Step> subgraphStack = new ArrayDeque<>();
+        private final Deque<String> subgraphStack = new ArrayDeque<>();
 
-        private String lastElement(GraphPath path ) {
-            return path.lastElement().orElse("__root__");
-        }
-
-        protected Optional<Step> isSubgraphEnded(RunnableConfig config) {
-            var isSubgraphEnded = !subgraphStack.isEmpty() &&
-                    Objects.equals( subgraphStack.peek().lastPathElement(), lastElement(config.graphPath()));
-            if( isSubgraphEnded ) {
-                return Optional.of(subgraphStack.pop());
+        protected Optional<String> isSubgraphEnded(RunnableConfig config) {
+            if( !subgraphStack.isEmpty() ) { // subgraph ended
+                return Optional.of( subgraphStack.pop() ) ;
             }
             return Optional.empty();
         }
 
-        protected Optional<Step> isSubgraphRequested(String nodeId, RunnableConfig config, Map<String,Object> result ) {
+        protected Optional<String> isSubgraphRequested(String nodeId, RunnableConfig config, Map<String,Object> result ) {
 
-            var isSubgraphRequested =  result.values().stream().anyMatch( v -> v instanceof AsyncGenerator<?>);
+            var isSubgraphRequested =  result.values().stream()
+                                        .anyMatch( v -> v instanceof AsyncGenerator<?>);
             if( isSubgraphRequested ) {
-                var item = new Step(nodeId, lastElement(config.graphPath()));
-                subgraphStack.push( item );
-                return Optional.of(item);
+                subgraphStack.push( nodeId );
+                return Optional.of( nodeId );
             }
             return Optional.empty();
         }
