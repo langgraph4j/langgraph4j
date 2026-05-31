2dea030836bd9451f27dea62f4ab810cb5f8ef6b
bsorrentino <bartolomeo.sorrentino@gmail.com>
2026-05-31 19:56:09 +0200

    feat(GraphResume): add checkpoint support

--- langgraph4j-core/src/main/java/org/bsc/langgraph4j/GraphResume.java
+++ langgraph4j-core/src/main/java/org/bsc/langgraph4j/GraphResume.java
@@ -1,18 +1,34 @@
 package org.bsc.langgraph4j;
 
+import org.bsc.langgraph4j.checkpoint.Checkpoint;
+import org.bsc.langgraph4j.utils.CollectionsUtils;
+
 import java.util.Map;
 
 import static java.util.Objects.requireNonNull;
 
 public record GraphResume(
-        Map<String,Object> value
+        Map<String,Object> value,
+        Checkpoint checkpoint
 ) implements GraphInput {
     public GraphResume {
         requireNonNull( value, "value cannot be null");
     }
-
-    public GraphResume() {
-        this(Map.of());
+    public GraphResume() {this(Map.of(), null); }
+    public GraphResume( Map<String,Object> value ) {this(value, null); }
+    public GraphResume( Checkpoint checkpoint ) {
+        this(Map.of(), checkpoint);
+    }
+    public GraphResume( Checkpoint checkpoint, Map<String,Object> value ) {
+        this(value, checkpoint);
     }
 
+    @Override
+    public String toString() {
+        return "GraphResume{ %s, %s }".formatted(
+                        CollectionsUtils.toString(value),
+                        checkpoint
+                );
+
+    }
 }
