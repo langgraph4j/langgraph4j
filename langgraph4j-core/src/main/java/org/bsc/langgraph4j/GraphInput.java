95655fe132ca7390228c8c96797a9b1f379ee655
bsorrentino <bartolomeo.sorrentino@gmail.com>
2026-05-31 19:56:31 +0200

    feat(GraphInput): add overloads for resume method to support Checkpoint

--- langgraph4j-core/src/main/java/org/bsc/langgraph4j/GraphInput.java
+++ langgraph4j-core/src/main/java/org/bsc/langgraph4j/GraphInput.java
@@ -1,5 +1,7 @@
 package org.bsc.langgraph4j;
 
+import org.bsc.langgraph4j.checkpoint.Checkpoint;
+
 import java.util.Map;
 
 public sealed interface GraphInput permits GraphArgs, GraphResume {
@@ -7,8 +9,12 @@ public sealed interface GraphInput permits GraphArgs, GraphResume {
     static GraphInput resume() {
         return new GraphResume();
     }
-    static GraphInput resume( Map<String,Object> value ) {
-        return new GraphResume(value);
+    static GraphInput resume( Map<String,Object> value ) { return new GraphResume(value); }
+    static GraphInput resume( Checkpoint checkpoint ) {
+        return new GraphResume(checkpoint);
+    }
+    static GraphInput resume( Checkpoint checkpoint, Map<String,Object> value ) {
+        return new GraphResume(checkpoint, value);
     }
 
     static GraphInput args( Map<String,Object> value) {
