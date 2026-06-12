4a4679540d5c47689a8de5c2637f55993f43a819
bsorrentino <bartolomeo.sorrentino@gmail.com>
2026-06-12 21:12:20 +0200

    fix(spring-ai-agent): reintroduce default system message
    work on #410

--- spring-ai/spring-ai-agent/src/main/java/org/bsc/langgraph4j/spring/ai/agent/DefaultChatService.java
+++ spring-ai/spring-ai-agent/src/main/java/org/bsc/langgraph4j/spring/ai/agent/DefaultChatService.java
@@ -1,9 +1,15 @@
 package org.bsc.langgraph4j.spring.ai.agent;
 
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.SystemMessage;
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.model.tool.ToolCallingChatOptions;
+import reactor.core.publisher.Flux;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Optional;
 
 import static java.util.Objects.requireNonNull;
@@ -12,6 +18,7 @@ import static java.util.Optional.ofNullable;
 class DefaultChatService implements ReactAgent.ChatService {
     final ChatModel chatModel;
     final ChatOptions chatOptions;
+    final SystemMessage defaultSystem;
 
     public DefaultChatService(ReactAgentBuilder<?,?> builder ) {
         this.chatModel = requireNonNull(builder.chatModel, "chatModel cannot be null!");
@@ -22,10 +29,15 @@ class DefaultChatService implements ReactAgent.ChatService {
         else {
             chatOptions = null;
         }
+
+        defaultSystem = SystemMessage.builder()
+                .text(builder.systemMessage().orElse(
+                        "You are a helpful AI Assistant answering questions." ))
+                .build() ;
     }
 
     @Override
-    public final ChatModel chatModel() {
+    public ChatModel chatModel() {
         return chatModel;
     }
 
@@ -34,4 +46,19 @@ class DefaultChatService implements ReactAgent.ChatService {
         return ofNullable(chatOptions);
     }
 
+    @Override
+    public ChatResponse execute(List<Message> messages) {
+        final var newMessages = new LinkedList<Message>();
+        newMessages.add(defaultSystem);
+        newMessages.addAll(messages);
+        return ReactAgent.ChatService.super.execute(newMessages);
+    }
+
+    @Override
+    public Flux<ChatResponse> streamingExecute(List<Message> messages) {
+        final var newMessages = new LinkedList<Message>();
+        newMessages.add(defaultSystem);
+        newMessages.addAll(messages);
+        return ReactAgent.ChatService.super.streamingExecute(newMessages);
+    }
 }
