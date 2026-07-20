f1fe7a5287561304c90ace14ffed9def17243855
bsorrentino <bartolomeo.sorrentino@gmail.com>
2026-07-20 15:44:58 +0200

    feat(SubGraphOutputFactory): enhance createFromNodeOutput to include RunnableConfig and metadata

--- langgraph4j-core/src/main/java/org/bsc/langgraph4j/subgraph/SubGraphOutputFactory.java
+++ langgraph4j-core/src/main/java/org/bsc/langgraph4j/subgraph/SubGraphOutputFactory.java
@@ -1,20 +1,26 @@
 package org.bsc.langgraph4j.subgraph;
 
 import org.bsc.langgraph4j.NodeOutput;
+import org.bsc.langgraph4j.RunnableConfig;
+import org.bsc.langgraph4j.metadata.MetadataBag;
 import org.bsc.langgraph4j.state.AgentState;
 import org.bsc.langgraph4j.state.StateSnapshot;
 
 public interface SubGraphOutputFactory {
 
-    static <State extends AgentState> SubGraphOutput<State> createFromNodeOutput(NodeOutput<State> output, String subGraphId ) {
+    static <State extends AgentState> SubGraphOutput<State> createFromNodeOutput(NodeOutput<State> output, String subGraphId, RunnableConfig config ) {
         if( output instanceof SubGraphOutput<State> subGraphOutput) {
             return subGraphOutput;
         }
         else {
+            final var nodePath = config.nodePath().replaceLast(output.node());
+            final var metadata = MetadataBag.builder()
+                    .putMetadata( RunnableConfig.GRAPH_NODE_PATH, nodePath)
+                    .build();
             if( output instanceof StateSnapshot<State> subGraphSnapshotOutput ) {
-                return new SubGraphSnapshotOutput<>( subGraphSnapshotOutput, subGraphId );
+                return new SubGraphSnapshotOutput<>( subGraphSnapshotOutput, subGraphId, metadata );
             }
-            return new SubGraphOutput<>( output, subGraphId );
+            return new SubGraphOutput<>( output, subGraphId, metadata );
         }
 
     }
