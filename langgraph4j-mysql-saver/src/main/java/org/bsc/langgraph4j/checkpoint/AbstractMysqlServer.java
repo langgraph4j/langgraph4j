1c4f510adb4460ac51d9a3fb9aeb168a7bf3d28f
bsorrentino <bartolomeo.sorrentino@gmail.com>
2026-08-12 15:22:45 +0200

    feat(MysqlServer): make compliant with new BaseCheckpointSaver adding default implementation of releaseCheckpointsOnError and registerInterruption methods
    work on #428

--- langgraph4j-mysql-saver/src/main/java/org/bsc/langgraph4j/checkpoint/AbstractMysqlServer.java
+++ langgraph4j-mysql-saver/src/main/java/org/bsc/langgraph4j/checkpoint/AbstractMysqlServer.java
@@ -1,14 +1,20 @@
 package org.bsc.langgraph4j.checkpoint;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.bsc.langgraph4j.LG4JLoggable;
 import org.bsc.langgraph4j.RunnableConfig;
+import org.bsc.langgraph4j.action.InterruptionMetadata;
+import org.bsc.langgraph4j.state.AgentState;
 import org.bsc.langgraph4j.utils.TryFunction;
 
 import javax.sql.DataSource;
 import java.sql.*;
 import java.util.*;
+import java.util.concurrent.CompletableFuture;
 
-public abstract class AbstractMysqlServer extends AbstractCheckpointSaver {
+import static java.util.concurrent.CompletableFuture.completedFuture;
+
+public abstract class AbstractMysqlServer extends AbstractCheckpointSaver implements LG4JLoggable {
     // DDL statements
     private static final String CREATE_THREAD_TABLE = """
             CREATE TABLE IF NOT EXISTS LANGRAPH4J_THREAD (
@@ -222,7 +228,7 @@ public abstract class AbstractMysqlServer extends AbstractCheckpointSaver {
      *                   released
      */
     @Override
-    protected Tag releaseCheckpoints(RunnableConfig config, LinkedList<Checkpoint> checkpoints) throws Exception {
+    protected Tag releaseCheckpoints(RunnableConfig config, LinkedList<Checkpoint> checkpoints, String message) throws Exception {
         final String threadName = threadId(config);
 
         try (Connection connection = dataSource.getConnection();
@@ -234,6 +240,17 @@ public abstract class AbstractMysqlServer extends AbstractCheckpointSaver {
         }
 
         return new Tag( threadName, checkpoints);
+    }
+
+
+    @Override
+    protected Tag releaseCheckpointsOnError(RunnableConfig config, LinkedList<Checkpoint> checkpoints, Exception exception) throws Exception {
+        return releaseCheckpoints(config, checkpoints, exception.getMessage());
+    }
+
+    @Override
+    public <State extends AgentState> CompletableFuture<InterruptionMetadata<State>> registerInterruption(RunnableConfig config, InterruptionMetadata<State> interruptionMetadata) {
+        return completedFuture(interruptionMetadata);
     }
 
     /**
