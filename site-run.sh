1e0fd6e383595fc7c2ad5a723c5449855a59c475
bsorrentino <bartolomeo.sorrentino@gmail.com>
2026-08-27 22:34:15 +0200

    docs: update integration links to point to README files

--- site-run.sh
+++ site-run.sh
@@ -12,6 +12,21 @@ cp -r src/site/mkdocs target/mkdocs
 cp how-tos/*.ipynb target/mkdocs/how-tos
 cp -r target/staging/apidocs target/mkdocs/apidocs
 
+# Copy Spring AI docs
+rsync -avm \
+  --include='*/' \
+  --include='README.md' \
+  --exclude='*' \
+  spring-ai/ target/mkdocs/integrations/spring-ai/
+
+# Copy LangChain4j docs
+rsync -avm \
+  --include='*/' \
+  --include='README.md' \
+  --exclude='*' \
+  langchain4j/ target/mkdocs/integrations/langchain4j/
+
+
 source .docsenv/bin/activate
 
 mkdocs build
