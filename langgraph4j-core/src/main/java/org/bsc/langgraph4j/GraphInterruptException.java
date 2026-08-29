4e3ae199eb620ef9d3d18597035ff74c5292061f
bsorrentino <bartolomeo.sorrentino@gmail.com>
2026-08-29 23:10:16 +0200

    feat: add static method to retrieve GraphInterruptException from throwable

--- langgraph4j-core/src/main/java/org/bsc/langgraph4j/GraphInterruptException.java
+++ langgraph4j-core/src/main/java/org/bsc/langgraph4j/GraphInterruptException.java
@@ -1,12 +1,20 @@
 package org.bsc.langgraph4j;
 
 
+import org.bsc.langgraph4j.utils.ExceptionUtils;
+
+import java.util.Optional;
+
 /**
  * Exception thrown when the execution of a graph is interrupted.
  *
  * @since 1.9.0-beta4
  */
 public class GraphInterruptException extends GraphRunnerException {
+
+    public static Optional<GraphInterruptException> of(Throwable throwable) {
+        return ExceptionUtils.findCauseByType(throwable, GraphInterruptException.class);
+    }
 
     public GraphInterruptException(RunnableConfig config, String reason) {
         super(config, reason);
