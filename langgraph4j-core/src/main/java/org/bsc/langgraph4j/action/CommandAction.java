51fe10c308216432d138a9cd5afd3563d977a399
TianhengZhuang <39034691+TianHengZhuang@users.noreply.github.com>
2026-09-04 12:10:35 +0800

    docs: add Javadoc to NodeAction and CommandAction interfaces

--- langgraph4j-core/src/main/java/org/bsc/langgraph4j/action/CommandAction.java
+++ langgraph4j-core/src/main/java/org/bsc/langgraph4j/action/CommandAction.java
@@ -3,6 +3,18 @@ package org.bsc.langgraph4j.action;
 import org.bsc.langgraph4j.RunnableConfig;
 import org.bsc.langgraph4j.state.AgentState;
 
+/**
+ * Defines a node that can also send command signals (goto, interrupt, etc.)
+ * in addition to updating state.
+ *
+ * <p>A CommandAction is used when a node needs to direct the graph execution
+ * to a specific next node, interrupt the execution, or otherwise control
+ * the traversal of the graph beyond simple state updates.
+ *
+ * @param <S> the state type, which must extend {@link AgentState}
+ * @see NodeAction
+ * @see Command
+ */
 @FunctionalInterface
 public interface CommandAction<S extends AgentState> {
     Command apply(S state, RunnableConfig config) throws Exception;
