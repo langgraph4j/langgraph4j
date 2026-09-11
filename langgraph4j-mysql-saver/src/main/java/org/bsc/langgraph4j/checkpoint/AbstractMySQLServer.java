8c372a0bdae2d90eec612a4a45e502b4e582e058
bsorrentino <bartolomeo.sorrentino@gmail.com>
2026-09-11 19:53:23 +0200

    refactor: rename AbstractMysqlServer to AbstractMySQLServer and improve SQL command execution

--- langgraph4j-mysql-saver/src/main/java/org/bsc/langgraph4j/checkpoint/AbstractMySQLServer.java
+++ langgraph4j-mysql-saver/src/main/java/org/bsc/langgraph4j/checkpoint/AbstractMySQLServer.java
@@ -3,22 +3,18 @@ package org.bsc.langgraph4j.checkpoint;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.bsc.langgraph4j.LG4JLoggable;
 import org.bsc.langgraph4j.RunnableConfig;
-import org.bsc.langgraph4j.action.InterruptionMetadata;
 import org.bsc.langgraph4j.serializer.StateSerializer;
 import org.bsc.langgraph4j.state.AgentState;
 import org.bsc.langgraph4j.utils.SqlResource;
+import org.bsc.langgraph4j.utils.TryFunction;
 import org.jspecify.annotations.Nullable;
 
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.sql.*;
 import java.util.*;
-import java.util.concurrent.CompletableFuture;
-import java.util.stream.Stream;
 
-import static java.util.concurrent.CompletableFuture.completedFuture;
-
-public abstract class AbstractMysqlServer extends AbstractCheckpointSaver implements LG4JLoggable {
+public abstract class AbstractMySQLServer extends AbstractCheckpointSaver implements LG4JLoggable {
 
     /**
      * A builder for MysqlSaver.
@@ -30,8 +26,9 @@ public abstract class AbstractMysqlServer extends AbstractCheckpointSaver implem
 
         @SuppressWarnings("unchecked")
         private B this$() {
-            return (B)this;
+            return (B) this;
         }
+
         /**
          * Sets the datasource
          *
@@ -70,32 +67,37 @@ public abstract class AbstractMysqlServer extends AbstractCheckpointSaver implem
     protected final DataSource dataSource;
     protected final CreateOption createOption;
     protected final ObjectMapper objectMapper;
-    protected final  StateSerializer<? extends AgentState> stateSerializer;
+    protected final StateSerializer<? extends AgentState> stateSerializer;
     protected final SqlResource.Commands sqlCommands;
+
     /**
      * protected constructor used by the builder to create a new instance of
      * MysqlSaver.
      *
-     * @param builder   Builder instance
+     * @param builder Builder instance
      */
-    protected AbstractMysqlServer(AbstractBuilder<? > builder) throws Exception {
+    protected AbstractMySQLServer(AbstractBuilder<?> builder) throws Exception {
         this.dataSource = builder.dataSource;
         this.createOption = builder.createOption;
         this.stateSerializer = builder.stateSerializer;
-        this.objectMapper = ( builder.stateSerializer == null) ? new ObjectMapper() : null;
-        this.sqlCommands = SqlResource.Commands.load("db/v1.1__commands.sql");
+        this.objectMapper = (builder.stateSerializer == null) ? new ObjectMapper() : null;
+        this.sqlCommands = SqlResource.Commands.load(sqlCommandsResourcePath());
         initTables();
     }
+
+    protected abstract String sqlCommandsResourcePath();
+
+    protected abstract String sqlInitResourcePath();
 
     /**
      * Initializes the database according the create options.
      */
     protected void initTables() throws Exception {
 
-        final var sqlInitCommands = SqlResource.Commands.load("db/migration/v1.1__init.sql");
+        final var sqlInitCommands = SqlResource.Commands.load(sqlInitResourcePath());
 
-            try (Connection connection = dataSource.getConnection();
-                 Statement statement = connection.createStatement()) {
+        execTransaction(connection -> {
+            try (var statement = connection.createStatement()) {
 
                 if (createOption == CreateOption.CREATE_OR_REPLACE) {
                     for (var sql : sqlCommands.getMultiple("sqlDropTables")) {
@@ -111,6 +113,8 @@ public abstract class AbstractMysqlServer extends AbstractCheckpointSaver implem
                     }
                 }
             }
+            return null;
+        });
     }
 
     private String encodeState(Map<String, Object> data) throws IOException {
@@ -128,13 +132,13 @@ public abstract class AbstractMysqlServer extends AbstractCheckpointSaver implem
 
     @SuppressWarnings("unchecked")
     private Map<String, Object> decodeState(String statePayload,
-                                            @Nullable  String stateContentType ) throws IOException, ClassNotFoundException {
+                                            @Nullable String stateContentType) throws IOException, ClassNotFoundException {
 
         if (stateSerializer == null) {
             return objectMapper.readValue(statePayload, Map.class);
         }
 
-        if (stateContentType!=null && !Objects.equals(stateContentType, stateSerializer.contentType())) {
+        if (stateContentType != null && !Objects.equals(stateContentType, stateSerializer.contentType())) {
             throw new IllegalStateException(
                     "Content Type used for stored state '%s' is different from one '%s' used to deserialize it".formatted(
                             stateContentType,
@@ -149,7 +153,7 @@ public abstract class AbstractMysqlServer extends AbstractCheckpointSaver implem
     /**
      * If the list of checkpoints is empty, loads the checkpoints from the database.
      *
-     * @param config      the configuration
+     * @param config the configuration
      * @return a list of checkpoints
      * @throws Exception if an error occurs while the checkpoints are being
      *                   loaded from the database.
@@ -161,25 +165,25 @@ public abstract class AbstractMysqlServer extends AbstractCheckpointSaver implem
         final var threadName = threadId(config);
 
         final var sqlSelectCheckpoints = sqlCommands.get("sqlSelectCheckpoints");
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(sqlSelectCheckpoints)) {
 
-            preparedStatement.setString(1, threadName);
-            try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                while (resultSet.next()) {
-                    Checkpoint checkpoint = Checkpoint.builder()
-                            .id(resultSet.getString(1))
-                            .nodeId(resultSet.getString(2))
-                            .nextNodeId(resultSet.getString(3))
-                            .state( decodeState(resultSet.getString(4), null))
-                            .build();
-                    checkpoints.add(checkpoint);
+        return exec(connection -> {
+            try (var preparedStatement = connection.prepareStatement(sqlSelectCheckpoints)) {
+
+                preparedStatement.setString(1, threadName);
+                try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                    while (resultSet.next()) {
+                        Checkpoint checkpoint = Checkpoint.builder()
+                                .id(resultSet.getString(1))
+                                .nodeId(resultSet.getString(2))
+                                .nextNodeId(resultSet.getString(3))
+                                .state(decodeState(resultSet.getString(4), null))
+                                .build();
+                        checkpoints.add(checkpoint);
+                    }
                 }
             }
-        } catch (SQLException sqlException) {
-            throw new Exception("Unable to load checkpoints", sqlException);
-        }
-        return checkpoints;
+            return checkpoints;
+        });
     }
 
     /**
@@ -201,36 +205,34 @@ public abstract class AbstractMysqlServer extends AbstractCheckpointSaver implem
         final var sqlLastInsertId = sqlCommands.get("sqlUpsertThread_last_insert_id");
         final var sqlInsertCheckpoint = sqlCommands.get("sqlInsertCheckpoint");
 
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement upsertStatement = connection.prepareStatement(sqlUpsertThread);
-             PreparedStatement lastInsertId = connection.prepareStatement(sqlLastInsertId);
-             PreparedStatement insertCheckpointStatement = connection.prepareStatement(sqlInsertCheckpoint)) {
+        execTransaction(connection -> {
+            try (var upsertStatement = connection.prepareStatement(sqlUpsertThread);
+                 var lastInsertId = connection.prepareStatement(sqlLastInsertId);
+                 var insertCheckpointStatement = connection.prepareStatement(sqlInsertCheckpoint)) {
 
-            upsertStatement.setString(1, threadName);
-            upsertStatement.execute();
+                upsertStatement.setString(1, threadName);
+                upsertStatement.execute();
 
-            long threadKey = -1;
-            try( ResultSet rs = lastInsertId.executeQuery() ) {
-                if( rs.next() ) {
-                    threadKey = rs.getLong(1);
-                    log.trace( "threadId {} for thread {}",threadKey, threadName);
+                long threadKey = -1;
+                try (ResultSet rs = lastInsertId.executeQuery()) {
+                    if (rs.next()) {
+                        threadKey = rs.getLong(1);
+                        log.trace("threadId {} for thread {}", threadKey, threadName);
+                    }
                 }
-            };
 
-
-            var index = 0;
-            insertCheckpointStatement.setString(++index, checkpoint.getId());
-            insertCheckpointStatement.setNull(++index, Types.VARCHAR);
-            insertCheckpointStatement.setLong(++index, threadKey);
-            insertCheckpointStatement.setString(++index, checkpoint.getNodeId());
-            insertCheckpointStatement.setString(++index, checkpoint.getNextNodeId());
-            insertCheckpointStatement.setString(++index, encodeState(checkpoint.getState()));
-            insertCheckpointStatement.setNull(++index, Types.VARCHAR);
-            insertCheckpointStatement.execute();
-        } catch (SQLException sqlException) {
-            throw new RuntimeException("Unable to insert checkpoint", sqlException);
-        }
-
+                var index = 0;
+                insertCheckpointStatement.setString(++index, checkpoint.getId());
+                insertCheckpointStatement.setNull(++index, Types.VARCHAR);
+                insertCheckpointStatement.setLong(++index, threadKey);
+                insertCheckpointStatement.setString(++index, checkpoint.getNodeId());
+                insertCheckpointStatement.setString(++index, checkpoint.getNextNodeId());
+                insertCheckpointStatement.setString(++index, encodeState(checkpoint.getState()));
+                insertCheckpointStatement.setNull(++index, Types.VARCHAR);
+                insertCheckpointStatement.execute();
+            }
+            return null;
+        });
     }
 
     /**
@@ -244,35 +246,18 @@ public abstract class AbstractMysqlServer extends AbstractCheckpointSaver implem
     @Override
     protected Tag releaseCheckpoints(RunnableConfig config, LinkedList<Checkpoint> checkpoints, String message) throws Exception {
         final String threadName = threadId(config);
-
         final var sqlReleaseThread = sqlCommands.get("sqlReleaseThread");
-        try (Connection connection = dataSource.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(sqlReleaseThread)) {
-            preparedStatement.setString(1, threadName);
-            preparedStatement.execute();
-        } catch (SQLException sqlException) {
-            throw new Exception("Unable to release checkpoint", sqlException);
-        }
 
-        return new Tag( threadName, checkpoints);
+        return exec(connection -> {
+
+            try (var preparedStatement = connection.prepareStatement(sqlReleaseThread)) {
+                preparedStatement.setString(1, threadName);
+                preparedStatement.execute();
+            }
+            return new Tag(threadName, checkpoints);
+        });
     }
 
-
-    @Override
-    protected Tag releaseCheckpointsOnError(RunnableConfig config, LinkedList<Checkpoint> checkpoints, Exception exception) throws Exception {
-        return releaseCheckpoints(config, checkpoints, exception.getMessage());
-    }
-
-    @Override
-    public <State extends AgentState> CompletableFuture<InterruptionMetadata<State>> registerInterruption(RunnableConfig config, InterruptionMetadata<State> interruptionMetadata) {
-        return completedFuture(interruptionMetadata);
-    }
-
-
-    @Override
-    public Optional<Tag> tag(RunnableConfig config, Integer version) throws Exception {
-        return Optional.empty();
-    }
 
     /**
      * If the checkpoint exists, updates the checkpoint, otherwise it inserts it.
@@ -284,37 +269,51 @@ public abstract class AbstractMysqlServer extends AbstractCheckpointSaver implem
      *                   checkpoint.
      */
     @Override
-    protected void updatedCheckpoint(RunnableConfig config, LinkedList<Checkpoint> checkpoints, Checkpoint checkpoint)
-            throws Exception {
-        if (config.checkPointId().isPresent()) {
-            final var sqlUpdateCheckpoint = sqlCommands.get("sqlUpdateCheckpoint");
-            try (Connection connection = dataSource.getConnection();
-                 PreparedStatement preparedStatement = connection.prepareStatement(sqlUpdateCheckpoint)) {
-                preparedStatement.setString(1, checkpoint.getId());
-                preparedStatement.setString(2, checkpoint.getNodeId());
-                preparedStatement.setString(3, checkpoint.getNextNodeId());
-                preparedStatement.setString(4, encodeState(checkpoint.getState()));
-                preparedStatement.setString(5, config.checkPointId().get());
-                preparedStatement.execute();
-            } catch (SQLException sqlException) {
-                throw new Exception("Unable to update checkpoint", sqlException);
+    protected void updatedCheckpoint(RunnableConfig config, LinkedList<Checkpoint> checkpoints, Checkpoint checkpoint) throws Exception {
+
+        execTransaction(connection -> {
+            if (config.checkPointId().isPresent()) {
+                final var sqlUpdateCheckpoint = sqlCommands.get("sqlUpdateCheckpoint");
+
+                try (var preparedStatement = connection.prepareStatement(sqlUpdateCheckpoint)) {
+                    preparedStatement.setString(1, checkpoint.getId());
+                    preparedStatement.setString(2, checkpoint.getNodeId());
+                    preparedStatement.setString(3, checkpoint.getNextNodeId());
+                    preparedStatement.setString(4, encodeState(checkpoint.getState()));
+                    preparedStatement.setString(5, config.checkPointId().get());
+                    preparedStatement.execute();
+                }
+            } else {
+                insertedCheckpoint(config, checkpoints, checkpoint);
             }
-        } else {
-            insertedCheckpoint(config, checkpoints, checkpoint);
-        }
+            return null;
+        });
     }
 
+    protected final <R> R exec(TryFunction<Connection, R, Exception> execStatement) throws Exception {
+        final var connection = dataSource.getConnection();
 
-        /**
-         * Removes the cached checkpoints associated with the given thread identifier from the in-memory cache.
-         *
-         * @param threadId the thread identifier whose cached checkpoints must be cleared
-         * @return the checkpoints removed from the cache, or an empty collection if no cached checkpoints exist
-         * @deprecated this method do nothing because currently this saver don't use cache anymore
-         */
-    @Deprecated(forRemoval = true)
-    public Collection<Checkpoint> clearCheckpointsCache( String threadId ) {
-        return List.of();
+        connection.setAutoCommit(true);
+
+        return execStatement.tryApply(connection);
+    }
+
+    protected final <R> R execTransaction(TryFunction<Connection, R, Exception> execStatement) throws Exception {
+        final var connection = dataSource.getConnection();
+
+        final var previousAutoCommit = connection.getAutoCommit();
+
+        connection.setAutoCommit(false);
+        try {
+            return execStatement.tryApply(connection);
+        } catch (Exception e) {
+            log.error("Error executing statement", e);
+            connection.rollback();
+            throw e;
+        } finally {
+            connection.commit();
+            connection.setAutoCommit(previousAutoCommit);
+        }
     }
 
 
