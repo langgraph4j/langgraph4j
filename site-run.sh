59ee0db1cf49576c5d51d64b6a448a29dd39b40d
bsorrentino <bartolomeo.sorrentino@gmail.com>
2026-09-12 16:29:17 +0200

    docs(oracle-saver): update Oracle checkpoint saver documentation

--- site-run.sh
+++ site-run.sh
@@ -36,6 +36,14 @@ rsync -avm \
 --exclude='*' \
 langgraph4j-mysql-saver/ target/mkdocs/core/mysql-saver/
 
+# Copy Oracle Checkpoint Saver docs
+rsync -avm \
+--include='*/' \
+--include='README.md' \
+--include='SAVER*.md' \
+--exclude='*' \
+langgraph4j-oracle-saver/ target/mkdocs/core/oracle-saver/
+
 
 # Copy Spring AI docs
 rsync -avm \
